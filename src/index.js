66b49571f09f2c300cccdfe60e8afe98f5814c28
Vu Anh Hao <hi@haova.me>
2022-08-02 21:05:05 +0700

    New auth version

--- src/index.js
+++ src/index.js
@@ -1,65 +1,9 @@
-import { wrapComposer } from '@rugo-vn/common';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
+import { clone, path } from 'ramda';
 
-/**
- * @typedef {object} Result
- * @property {number} status Status code.
- * @property {*} data Message or any data.
- */
+export const name = 'auth';
 
-/**
- * Return forbidden result.
- *
- * @param {*} data Message or data to response.
- * @returns {Result} return data.
- */
-export const forbidden = (data) => ({
-  status: 403,
-  data
-});
-
-/**
- * Return bad request result.
- *
- * @param {*} data Message or data to response.
- * @returns {Result} return data.
- */
-export const badRequest = (data) => ({
-  status: 400,
-  data
-});
-
-export const createLogin = wrapComposer(async (secret, model, form) => {
-  const { email, password } = form;
-
-  if (!email || !password) { return badRequest('Email and Password must not be empty'); }
-
-  const { data: { 0: info } } = await model.list({ $limit: 1, email });
-
-  if (!info || !bcrypt.compareSync(password, info.password)) {
-    return forbidden('Wrong email or password');
-  }
-
-  const token = jwt.sign({
-    id: info._id
-  }, secret, {
-    expiresIn: '30d'
-  });
-
-  return {
-    status: 200,
-    data: token
-  };
-});
-
-/**
- * Verify token
- *
- * @param {string} token token to verify
- * @param {string} secret secret of token
- * @returns {object} return object or false
- */
 export const verifyToken = (token, secret) => {
   try {
     return jwt.verify(token, secret);
@@ -68,30 +12,119 @@ export const verifyToken = (token, secret) => {
   }
 };
 
-export const createGate = wrapComposer(async (secret, model, disabled, token, context) => {
-  if (disabled) { return null; }
+export const actions = {
+  async login (ctx) {
+    const { form, identities } = ctx.params;
 
-  if (!token) { return forbidden('Access denied'); }
+    // make filters
+    const filters = identities.map(field => {
+      const filter = {};
+      filter[field] = form.identity;
+      return filter;
+    });
 
-  const [authType, authToken] = token.split(' ');
-  let user;
+    // try get user
+    let user;
+    for (const filter of filters) {
+      const res = await ctx.call('model.find', { filters: filter, limit: 1 });
+      if (!res.data[0]) { continue; }
 
-  if (authType === 'Bearer') {
-    const rel = await verifyToken(authToken, secret);
-    if (rel) {
-      user = await model.get(rel.id);
-    } else {
-      return forbidden('Your session has expired. Please logging in again!');
+      user = res.data[0];
+      break;
     }
-  } else {
-    user = (await model.list({
-      apikey: token
-    })).data[0];
+
+    if (!user) { return this.forbidden(); }
+
+    // compare password
+    if (!bcrypt.compareSync(form.password, user.password)) {
+      return this.forbidden();
+    }
+
+    // sign
+    const token = jwt.sign({
+      id: user._id
+    }, this.settings.secret, {
+      expiresIn: '30d'
+    });
+
+    return {
+      status: 200,
+      body: {
+        status: 'success',
+        data: token
+      }
+    };
+  },
+
+  async gate (ctx) {
+    // get token
+    const token = path(['params', 'headers', 'authorization'], ctx);
+
+    if (!token) { return this.forbidden(); }
+
+    const [authType, authToken] = token.split(' ');
+    let user;
+
+    // validate token
+    if (authType === 'Bearer') {
+      const rel = await verifyToken(authToken, this.settings.secret);
+      if (rel) {
+        const res = await ctx.call('model.get', { id: rel.id });
+        if (res.data) { user = res.data; }
+      }
+    }
+
+    if (!user) { return this.forbidden(); }
+
+    // validate permissions
+    const metaPerm = ctx.meta.perm || {};
+
+    if (Object.keys(metaPerm).length === 0) { return null; }
+
+    for (const userPerm of user.perms || []) {
+      let isMatch = true;
+
+      for (const key in metaPerm) {
+        if (userPerm[key] === '*') { continue; }
+
+        if (userPerm[key] === metaPerm[key]) { continue; }
+
+        isMatch = false;
+        break;
+      }
+
+      if (isMatch) {
+        return null;
+      }
+    }
+
+    return this.forbidden();
   }
+};
 
-  if (!user) { return forbidden('Access denied'); }
+export const hooks = {
+  before: {
+    async '*' ({ meta, params }) {
+      meta.schema = clone(meta.authSchema);
 
-  context.rugoUser = user;
+      if (!meta.schema) { throw new Error('authSchema must be defined'); }
 
-  return null;
-});
+      const identityFields = ['_id', ...(meta.schema.identities || [])];
+      delete meta.schema.identities;
+
+      params.identities = identityFields;
+    }
+  }
+};
+
+export const methods = {
+  forbidden () {
+    return {
+      status: 403,
+      body: {
+        status: 'error',
+        data: [{ type: 'general', message: 'forbidden' }]
+      }
+    };
+  }
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,86 +1,159 @@
 /* eslint-disable */
 
-import { expect } from 'chai';
-
-import { BaseComposer, KoaComposer } from "@rugo-vn/common";
-import { createGate, createLogin } from '../src/index.js';
-
 import bcrypt from 'bcryptjs';
-import { PASSWORD_SALT } from '@rugo-vn/common';
+import { expect } from 'chai';
+import { ServiceBroker } from 'moleculer';
+import { mergeDeepRight } from 'ramda';
 
-const FakeModel = {
-  list(query){
-    const successRes = { data: [{ _id: 'demoId', password: bcrypt.hashSync('123456', PASSWORD_SALT) }] };
+import * as authService from '../src/index.js';
 
-    if (query.email === 'admin@rugo.vn')
-      return successRes;
-
-    if (query.apikey === 'apikey')
-      return successRes;
-
-    return { data: [] };
-  },
-
-  get(id){
-    return id === 'demoId' ? { _id: 'demoId' } : null;
+const TEST_EMAIL = 'test@email.com';
+const TEST_PHONE = '0123456789';
+const TEST_PASSWORD = 'thisisasecretstring';
+const FAIL_PASSWORD = 'failpassword';
+const TEST_ID = 'thisisanid';
+const TEST_SETTINGS = {
+  meta: {
+    authSchema: {
+      name: 'users',
+      driver: 'mem',
+      properties: {},
+      identities: ['email', 'phone']
+    }
   }
 }
 
-describe('Auth test', () => {
-  it('should be created login and gate with base composer', async () => {
-    const login = createLogin(BaseComposer);
+const modelService = {
+  name: 'model',
+  actions: {
+    async get({ params }){
+      if (params.id === TEST_ID)
+        return { status: 'success', data: {
+          perms: [
+            { 'foo': 'bar', 'abc': 'def' },
+            { 'foo': 'bar', 'sth': 'sbd', 'ghi': '*'}
+          ]
+        }};
+        
+      return { status: 'success', data: null };
+    },
 
-    const res = await login('secret', FakeModel, { email: 'admin@rugo.vn', password: '123456' })();
+    async find({ params }){
+      const { filters } = params;
+
+      if (filters.email === TEST_EMAIL || filters.phone === TEST_PHONE){
+        return {
+          status: 'success',
+          data: [
+            {
+              _id: TEST_ID,
+              password: bcrypt.hashSync(TEST_PASSWORD, 10)
+            }
+          ]
+        }
+      }
+
+      return {
+        status: 'success',
+        data: []
+      };
+    }
+  }
+}
+
+describe('Auth service test', () => {
+  let broker;
+
+  beforeEach(async () => {
+    broker = new ServiceBroker();
+
+    broker.createService(modelService);
+    broker.createService(mergeDeepRight(authService, {
+      settings: { secret: 'jwtsecret' }
+    }));
+
+    await broker.start();
+  });
+
+  afterEach(async () => {
+    await broker.stop();
+  });
+
+  it('should be login', async () => {
+    const res = await broker.call('auth.login', { form: {
+      identity: TEST_EMAIL,
+      password: TEST_PASSWORD
+    } }, TEST_SETTINGS);
+
     expect(res).to.has.property('status', 200);
-    expect(res).to.has.property('data');
+    expect(res.body).to.has.property('status', 'success');
+    expect(res.body).to.has.property('data');
 
-    const gate = createGate(BaseComposer);
-    const context = {};
-    const res2 = await gate('secret', FakeModel, false, `Bearer ${res.data}`, context)();
-    expect(res2).to.be.eq(null);
-    expect(context).to.has.property('rugoUser');
+    const res2 = await broker.call('auth.login', { form: {
+      identity: TEST_PHONE,
+      password: TEST_PASSWORD
+    } }, TEST_SETTINGS);
 
-    const context2 = {};
-    const res3 = await gate('secret', FakeModel, false, `apikey`, context2)();
+    expect(res2).to.has.property('status', 200);
+    expect(res2.body).to.has.property('status', 'success');
+    expect(res2.body).to.has.property('data');
+
+    const res3 = await broker.call(
+      'auth.gate', 
+      { headers: { authorization: `Bearer ${res2.body.data}` } }, 
+      mergeDeepRight(TEST_SETTINGS, {
+        meta: {
+          perm: { foo: 'bar', ghi: 'demo' }
+        }
+      })
+    );
+
     expect(res3).to.be.eq(null);
-    expect(context2).to.has.property('rugoUser');
-
-    const context3 = {};
-    const res4 = await gate('secret', FakeModel, true, undefined, context3)();
-    expect(res4).to.be.eq(null);
-    expect(context3).to.not.has.property('rugoUser');
   });
 
-  it('should forbidden and bad request', async () => {
-    const login = createLogin(BaseComposer);
+  it('should be not login', async () => {
+    const res = await broker.call('auth.login', { form: {
+      identity: TEST_EMAIL,
+      password: FAIL_PASSWORD
+    } }, TEST_SETTINGS);
 
-    const res = await login('secret', FakeModel, { email: 'noemail', password: '123456' })();
     expect(res).to.has.property('status', 403);
-    expect(res).to.has.property('data', 'Wrong email or password');
+    expect(res.body).to.has.property('status', 'error');
+    expect(res.body.data[0]).to.has.property('message', 'forbidden');
 
-    const res2 = await login('secret', FakeModel, { password: '123456' })();
-    expect(res2).to.has.property('status', 400);
-    expect(res2).to.has.property('data', 'Email and Password must not be empty');
+    // invalid token
+    const res2 = await broker.call(
+      'auth.gate', 
+      { headers: { authorization: `Bearer wrongtoken` } }, 
+      mergeDeepRight(TEST_SETTINGS, {
+        meta: {
+          perm: { ghi: 'demo', abc: 'def' }
+        }
+      })
+    );
 
-    const gate = createGate(BaseComposer);
-    const context = {};
-    const res3 = await gate('secret', FakeModel, false, `Bearer invalidtoken`, context)();
-    expect(res3).to.be.has.property('status', 403);
-    expect(res3).to.be.has.property('data', 'Your session has expired. Please logging in again!');
-    expect(context).to.not.has.property('rugoUser');
+    expect(res2).to.has.property('status', 403);
+    expect(res2.body).to.has.property('status', 'error');
+    expect(res2.body.data[0]).to.has.property('message', 'forbidden');
 
-    const context2 = {};
-    const res4 = await gate('secret', FakeModel, false, undefined, context2)();
-    expect(res4).to.be.has.property('status', 403);
-    expect(res4).to.be.has.property('data', 'Access denied');
-    expect(context2).to.not.has.property('rugoUser');
+    // invalid perm
+    const res3 = await broker.call('auth.login', { form: {
+      identity: TEST_PHONE,
+      password: TEST_PASSWORD
+    } }, TEST_SETTINGS);
 
-    const context3 = {};
-    const res5 = await gate('secret', FakeModel, false, ' failapikey', context3)();
-    expect(res5).to.be.has.property('status', 403);
-    expect(res5).to.be.has.property('data', 'Access denied');
-    expect(context3).to.not.has.property('rugoUser');
+    const res4 = await broker.call(
+      'auth.gate', 
+      { headers: { authorization: `Bearer ${res3.body.data}` } }, 
+      mergeDeepRight(TEST_SETTINGS, {
+        meta: {
+          perm: { ghi: 'demo', abc: 'def' }
+        }
+      })
+    );
+
+    expect(res4).to.has.property('status', 403);
+    expect(res4.body).to.has.property('status', 'error');
+    expect(res4.body.data[0]).to.has.property('message', 'forbidden');
   });
-  
 });
-
